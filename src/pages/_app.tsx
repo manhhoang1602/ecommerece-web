efe4b7996cf127d18b45fafb58cb496186cc0910
manhhoang1602 <manhhoang1602@gmail.com>
2023-01-27 15:17:36 +0700

    set font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Open_Sans } from '@next/font/google'
+
+const OpenSans = Open_Sans({
+  weight: ['400', '700'],
+  subsets: ['vietnamese'],
+  style: ['normal', 'italic'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={OpenSans.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
